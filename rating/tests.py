2965cd012219706ca655d39e6dc190ab25fbc239
SheilaBirgen <jeronobergen@gmail.com>
2020-03-13 10:45:39 +0300

    Add a testclass for signup

--- rating/tests.py
+++ rating/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from .views import signup
 # Create your tests here.
+class SignUpTests(TestCase):
+    def test_signup_status_code(self):
+        url = reverse('signup')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+ .
