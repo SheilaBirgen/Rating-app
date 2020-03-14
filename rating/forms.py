64343db0949254d1ca30301c694afeea1d3bd97a
SheilaBirgen <jeronobergen@gmail.com>
2020-03-14 13:02:15 +0300

    Add class update form for user and prfofile in forms.py

--- rating/forms.py
+++ rating/forms.py
@@ -7,3 +7,17 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['pic', 'bio', 'contact_info']
--- rating/urls.py
+++ rating/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('registration/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('',views.home, name ='home'),
-    
+
 ]
