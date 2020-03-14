2f84559f997f99f7b193316f242e1873cfa25fb1
SheilaBirgen <jeronobergen@gmail.com>
2020-03-14 12:21:32 +0300

    add class project to models.py

--- rating/models.py
+++ rating/models.py
@@ -9,7 +9,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     photo = models.ImageField(upload_to = 'profiles/', blank=True, default='profiles/default.jpg')
-
+    contact_info = models.CharField(max_length = 20, blank=True)
+   
     def delete_profile(self):
         self.delete()
     
@@ -19,9 +20,39 @@ class Profile(models.Model):
     def save_profile(self):
         self.save()
 
+    @classmethod
+    def get_profiles(cls):
+        profile = cls.objects.all()
+        return profile
+
+
 class Project(models.Model):
     project_title = models.CharField(max_length=30)
     project_description = models.TextField(max_length=255)
     pub_date = models.DateTimeField(auto_now=True)
-    project_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,default='1')
+    project_image = models.ImageField(upload_to='images/')
+    project_link = models.CharField(max_length=150)
+    profile = models.ForeignKey(Profile,on_delete= models.CASCADE, default='1')
 
+
+    def __str__(self):
+        return f'{self.profile.user.username}'
+
+    @classmethod
+    def get_project_by_id(cls, id):
+        try:
+            proj = Projects.objects.get(pk=id)
+        except ObjectDoesNotExist:
+            raise Http404()
+        return proj
+
+    @classmethod
+    def get_projects(cls):
+        project = cls.objects.all()
+        return project
+
+    @classmethod
+    def search_by_title(cls, search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        return projects
