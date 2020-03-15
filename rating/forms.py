2edddf85e627a8911bd2e6d148c4aa0916b14ba7
SheilaBirgen <jeronobergen@gmail.com>
2020-03-15 10:30:49 +0300

    add urls to urls.py in rating app

--- rating/forms.py
+++ rating/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import *
+
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
@@ -21,3 +23,15 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['pic', 'bio', 'contact_info']
+
+
+class PostProjectForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        fields = ['image', 'title', 'description', 'link']
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Reviews
+        fields = ['design', 'usability', 'content']
--- rating/urls.py
+++ rating/urls.py
@@ -4,8 +4,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('registration/', views.signup, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('',views.home, name ='home'),
+    path('registration/', views.Registration, name='registration'),
+     path('profile/', views.Profile, name='profile'),
+    path('new_site/', views.post_project, name='new_site'),
+    path('project/<int:project_id>', views.get_project, name='project'),
+    path('search/', views.search_results, name='search'),
+    
 
 ]
