549e1404e21ee1d5daef17f34d4171165c7d03d4
SheilaBirgen <jeronobergen@gmail.com>
2020-03-24 02:13:53 +0300

    changed the spellimg error

--- rating/urls.py
+++ rating/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('new_site/', views.post_project, name='new_site'),
     path('project/<int:project_id>', views.get_project, name='project'),
     path('search/', views.search_results, name='search'),
-    path('api/projects/', views.ProjectView.as_view()),
+    path('api/project/', views.ProjectView.as_view()),
     path('api_token_auth/', obtain_auth_token),
     path('profile_api/', include(router.urls)),
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
